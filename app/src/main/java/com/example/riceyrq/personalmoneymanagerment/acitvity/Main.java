591adfc52d500f5d5629db4eb9947f70f5d3fc92
riceyrq <804733259@qq.com>
2018-07-08 14:19:34 +0800

    添加滑动呼出drawer，因为touch事件传递后没回来activity,所以使用了dispatch来处理手势，效果是一样的

--- app/src/main/java/com/example/riceyrq/personalmoneymanagerment/acitvity/Main.java
+++ app/src/main/java/com/example/riceyrq/personalmoneymanagerment/acitvity/Main.java
@@ -5,6 +5,9 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -14,13 +17,14 @@ import com.example.riceyrq.personalmoneymanagerment.acitvity.fragment.AddFragmen
 import com.example.riceyrq.personalmoneymanagerment.acitvity.fragment.MonthPicFragment;
 import com.example.riceyrq.personalmoneymanagerment.acitvity.fragment.ShowFragment;
 import com.example.riceyrq.personalmoneymanagerment.acitvity.fragment.YearPicFragment;
+import com.example.riceyrq.personalmoneymanagerment.util.ToastUtil;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
-public class Main extends Activity implements Drawer.OnDrawerItemClickListener{
+public class Main extends Activity implements Drawer.OnDrawerItemClickListener, GestureDetector.OnGestureListener{
 
     public Drawer drawer;
     private final long ABOUT = 1;
@@ -32,6 +36,7 @@ public class Main extends Activity implements Drawer.OnDrawerItemClickListener{
     private final long RESTORE = 7;
     private final long BACKUP = 8;
     private String username = "";
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,11 +90,24 @@ public class Main extends Activity implements Drawer.OnDrawerItemClickListener{
             displayFragment(new ShowFragment());
         }
 
+        gestureDetector = new GestureDetector(Main.this, Main.this);
 
 
 
 
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        gestureDetector.onTouchEvent(ev);
+        return super.dispatchTouchEvent(ev);
+    }
+
+   /* @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        ToastUtil.showToast(getApplicationContext(), "touch");
+        return gestureDetector.onTouchEvent(event);
+    }*/
 
     public void displayFragment(Fragment fragment){
         getFragmentManager().beginTransaction().replace(R.id.fragment_holder, fragment).commitAllowingStateLoss();
@@ -107,6 +125,8 @@ public class Main extends Activity implements Drawer.OnDrawerItemClickListener{
         drawer.saveInstanceState(outState);
 
     }
+
+
 
     @Override
     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
@@ -137,5 +157,54 @@ public class Main extends Activity implements Drawer.OnDrawerItemClickListener{
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        int FLIP_DISTANCE = 50;
+        if (e1.getX() - e2.getX() > FLIP_DISTANCE) {
+            //Log.i("test", "向左滑...");
+
+            return true;
+        }
+        if (e2.getX() - e1.getX() > FLIP_DISTANCE) {
+            //Log.i("test", "向右滑...");
+            drawer.openDrawer();
+            return true;
+        }
+        if (e1.getY() - e2.getY() > FLIP_DISTANCE) {
+            //Log.i("test", "向上滑...");
+            return true;
+        }
+        if (e2.getY() - e1.getY() > FLIP_DISTANCE) {
+           // Log.i("test", "向下滑...");
+            return true;
+        }
+            return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
